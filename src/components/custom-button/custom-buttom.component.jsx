dc4d99c1be56ec498b01c9a06e2cca0d1ab837aa
TA-CHUANG-CHEN <bignoodlepottage@gmail.com>
2021-04-14 17:33:17 +0800

    firebase store signin  component

--- src/components/custom-button/custom-buttom.component.jsx
+++ src/components/custom-button/custom-buttom.component.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./custom-buttom.style.scss";
 
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+  //  props.children === <CustomButton> children </CustomButton>
   <button
     className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`} // if true google-sign-in will work
     {...otherProps}
   >
     {children}
-  </button> // /* props.children === <CustomButton> children </CustomButton> */
+  </button>
 );
 
 export default CustomButton;
