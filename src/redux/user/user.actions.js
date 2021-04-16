c9cae06e647b4b9e2bee446370b376a91777244e
TA-CHUANG-CHEN <bignoodlepottage@gmail.com>
2021-04-16 15:45:59 +0800

    redux user.actions&&types

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,6 @@
+import { UserActionTypes } from "./user.types";
+
 export const setCurrentUser = (user) => ({
-  type: "SET_CURRENT_USER",
-  payload: user,
+  type: UserActionTypes.SET_CURRENT_USER, //tell  reducer what action it should take,
+  payload: user, // the data that your reducer will use to update the state
 });
