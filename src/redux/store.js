b02a2593bb5bb5793fbf68a516dcebc4608356ca
TA-CHUANG <bignoodlepottage@gmail.com>
2021-06-03 11:55:31 +0800

    make logger only launch in dev mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,8 +4,11 @@ import logger from "redux-logger"; //GUI
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger]; //logger is dispatch GUI log in dev.tool
+const middlewares = []; //logger is dispatch GUI log in dev.tool
 
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 // 1. store that holds the complete state tree of your app, There should only be a single store in your app.
