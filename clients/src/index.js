287d3788deb6db0b0ed014a5625d1103fd63fc14
TA-CHUANG-CHEN <bignoodlepottage@gmail.com>
2021-06-24 17:52:55 +0800

    build pwa

--- clients/src/index.js
+++ clients/src/index.js
@@ -4,8 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
-import * as serviceWorker from "./service-worker";
 import "./index.css";
+import * as serviceWorker from "./serviceWorkerRegistration";
 import App from "./App";
 
 ReactDOM.render(
